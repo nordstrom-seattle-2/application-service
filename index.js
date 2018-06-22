15941692d4f00abe5b8310fd0a748e6fc9e6165f
Valliere, Andrea <andrea.valliere@nordstrom.com>
2018-06-21 17:09:26 -0700

    Added dynamo provisioning and  endpoints

--- index.js
+++ index.js
@@ -1,9 +1,61 @@
 const serverless = require('serverless-http');
+const bodyParser = require('body-parser');
 const express = require('express')
 const app = express()
+const AWS = require('aws-sdk');
+
+const APPLICATIONS_TABLE = process.env.APPLICATIONS_TABLE
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+app.use(bodyParser.json({ strict: false }));
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
+
+app.get('/applications/:applicationId', function (req, res) {
+  const params = {
+    TableName: APPLICATIONS_TABLE,
+    Key: {
+      applicationId: req.params.applicationId
+    },
+  }
+
+  dynamoDb.get(params, (error, result) => {
+    if (error) {
+      console.log(error);
+      res.status(400).json({ error: 'Could not get application' });
+    }
+    if (result.Item) {
+      const {applicationId} = result.Item;
+      res.json({ applicationId });
+    } else {
+      res.status(404).json({ error: "Application not found" });
+    }
+  });
+})
+
+app.post('/applications', function (req, res) {
+  const { applicationId } = req.body;
+  if (typeof applicationId !== 'integer') {
+    res.status(400).json({ error: 'applicationId must be an integer' });
+  }
+
+  const params = {
+    TableName: APPLICATIONS_TABLE,
+    Item: {
+      applicationId: applicationId
+    },
+  };
+
+  dynamoDb.put(params, (error) => {
+    if (error) {
+      console.log(error);
+      res.status(400).json({ error: 'Could not create application' });
+    }
+    res.json({ applicationId });
+  });
+})
+
 
 module.exports.handler = serverless(app);
